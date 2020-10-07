3db42bdbe8df755af5a5dbdb8fb7b34b7ff37209
Jesal Mehta <jesaldmehta@gmail.com>
2020-10-07 16:24:47 -0400

    Defined the various arrays

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function writePassword() {
 
 // Define all the character type groups as arrays
       //Array for lowerCase
-
+      var lowerCaseAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
       //Array for upperCase
-
+      var upperCaseAlpha = ['A', 'B', 'C', 'D',	'E', 'F', 'G', 'H', 'I', 'J',	'K', 'L', 'M', 'N', 'O','P', 'Q',	'R', 'S',	'T', 'U',	'V','W','X', 'Y', 'Z'];
       //Array for specialCharacters
-
+      var specialChar = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
       //Array for numbers
-
+      var numbers = ['0',	'1',	'2',	'3',	'4',	'5',	'6',	'7',	'8',	'9']
 
 // Prompt the user for password criteria desired
       // Prompt to enter the desired password length
@@ -28,15 +28,26 @@ function writePassword() {
 
 
       // Prompt/Confirm with the user which character types does he want to include in the password
-      // Loop all the character type questions using an array
+      // Loop all the character type questions using an array. 
+        // Created an array of objects with questions as a property, to leave room for adding the user responses as another property that will be needed in future
+        var charTypeQuestions = [
+          {q:'Do you want to include lower case letters in your password?'},
+          {q:'Do you want to include upper case letters in your password?'},
+          {q:'Do you want to include special characters in your password?'},
+          {q:'Do you want to include numbers in your password?'}
+        ]
 
+        // Loop the questions using for-loop
+        
+        
 
+        
 // Validate that at least one character type has been selected by the user
 
 
 // Create an array with possible characters based on user response of the desired character types to be included
       // Define an empty array
-
+      var possibleChar=[];
 
       // Push/concatenate the arrays of the desired character types into the new array created
 
